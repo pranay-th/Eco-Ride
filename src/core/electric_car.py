11d97c7d179e7aafd54ce36e123505cfa5043176
Pranay <pranayisgreat11@gmail.com>
2026-03-23 16:20:37 +0530

    Changed validation check for child variable

--- src/core/electric_car.py
+++ src/core/electric_car.py
@@ -3,7 +3,7 @@ from .vehicle import Vehicle
 class ElectricCar(Vehicle):
     def __init__(self,vehicle_id,model,battery_percentage,seating_capacity):
         super().__init__(vehicle_id,model,battery_percentage)
-        if seating_capacity<0:
+        if seating_capacity<=0:
             raise ValueError("Seating Capacity cannot be negative")
         else:
             self.seating_capacity = seating_capacity
--- src/core/electric_scooter.py
+++ src/core/electric_scooter.py
@@ -3,7 +3,7 @@ from .vehicle import Vehicle
 class ElectricScooter(Vehicle):
     def __init__(self, vehicle_id, model, battery_percentage, max_speed_limit):
         super().__init__(vehicle_id, model, battery_percentage)
-        if max_speed_limit<1:
+        if max_speed_limit<=0:
             raise ValueError("Max speed cannot be negative")
         else:
             self.max_speed_limit = max_speed_limit
