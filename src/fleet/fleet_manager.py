34dfe8950bbef21486916316c2b96f23e26068df
Pranay <pranayisgreat11@gmail.com>
2026-03-24 01:52:44 +0530

    Implemented class Fleet Manager to add/remove hubs and vehicles

---
+++ src/fleet/fleet_manager.py
@@ -0,0 +1,27 @@
+class FleetManager():
+    def __init__(self):
+        self.hubs = {}
+
+    def add_hub(self,hub_name):
+        if hub_name in self.hubs:
+            print(f"Hub {hub_name} already exists.")
+            return 
+        self.hubs[hub_name]=[]
+        print(f"Hub {hub_name} added!")
+
+    
+    def add_vehicle_to_hub(self,hub_name,vehicle):
+        if hub_name not in self.hubs:
+            print(f"Hub {hub_name} does not exist.")
+            return
+        self.hubs[hub_name].append(vehicle)
+        print(f"Vehicle {vehicle} added to Hub {hub_name} succesfully!")
+
+    def get_vehicles_by_hub(self,hub_name):
+        if hub_name not in self.hubs:
+            print(f"Hub {hub_name} does not exist.")
+            return []
+        return self.hubs[hub_name]
+
+    def list_all_hubs(self):
+        return list(self.hubs.keys())
